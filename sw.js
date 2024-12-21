548af387d2e13d70b8891c4fbca1574206f06331
care-inK <karenjeancartalaba@gmail.com>
2024-12-21 16:51:45 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "itunes-cache-v1";
+const assetsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "https://fonts.googleapis.com/css?family=Arial",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assetsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) return caches.delete(cache);
+        })
+      )
+    )
+  );
+});
